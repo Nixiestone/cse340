1e8d3ab56aad55eb41a1c6b78a8226879832edc2
Blessing Omoregie <omoregieblessing52@gmail.com>
2026-09-02 16:04:20 +0100

    feat: add categories page and update navigation

--- server.js
+++ server.js
@@ -36,6 +36,11 @@ app.get('/projects', async (req, res) => {
     res.render('projects', { title });
 });
 
+app.get('/categories', async (req, res) => {
+    const title = 'Service Project Categories';
+    res.render('categories', { title });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`Environment: ${NODE_ENV}`);
